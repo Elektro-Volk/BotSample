1f65780f6a836a695bec26bf551e924a8bba3525
TheEVolk <elektro-volk.ru>
2018-07-23 14:58:57 +0800

    Исправил Invites :D

--- scripts/commands/other/accept.lua
+++
@@ -1,3 +0,0 @@
-local command = { "принять", "Принять запрос", use = "<номер>", smile = '&#9989;' };
-function command.exe(msg, args, other, rmsg, user) return Invites.Do(msg, args, other, rmsg, user) end
-return command;
--- scripts/modules/Invites.lua
+++ scripts/modules/Invites.lua
@@ -1,15 +1,21 @@
 return {
 	queries = {},
 
+	Start = function ()
+		if not Commands then return end
+		Commands.commands["принять"] = { "принять", "Принять запрос", use = "<номер>", smile = '&#9989;', args = 'i', exe = Invites.Do, type = 'other' };
+		Commands.cmd_count = Commands.cmd_count + 1;
+	end,
+
 	Invite = function (user, target, callback, data, timer)
 		-- Очистить старые запросы юзера
-		for k,v in pairs(this.queries) do
-			if v.source_id == user.vkid then this.queries[k] = nil end
+		for k,v in pairs(Invites.queries) do
+			if v.source_id == user.vkid then Invites.queries[k] = nil end
 		end
 
 		local qid = math.random(8999) + 1000;
 
-		this.queries[qid] = {
+		Invites.queries[qid] = {
 			data = data,
 			endtime = (timer or 300) + os.time(),
 			target_id = target.vkid,
@@ -24,10 +30,10 @@ return {
 		local qid = tonumber(args[2]);
 		if not qid then return "err:используйте: принять <номер>" end
 
-		local query = this.queries[qid];
+		local query = Invites.queries[qid];
 		if not query then return "err:запрос не найден" end
 		if query.target_id ~= user.vkid then return "err:это не ваш запрос" end
-		this.queries[qid] = nil;
+		Invites.queries[qid] = nil;
 		if query.endtime < os.time() then return "err:время запроса истекло" end
 
 		return query.callback(query.data, user, DbData(query.source_id), rmsg);
